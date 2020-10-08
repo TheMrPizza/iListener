f82b7ce12df9a7cb7826f6e4b740557c46e9e8e5
Roy Gordon <royg2000@gmail.com>
2020-10-09 01:47:23 +0300

    quickfixz

--- ilistener/src/pages/homePage.js
+++ ilistener/src/pages/homePage.js
@@ -1,26 +1,30 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Feed from '../components/feed';
 import PostCreation from '../components/postCreation';
 import {getPosts, createPost} from '../services/postsService';
 import '../App.css';
+import { Button } from '@material-ui/core';
+
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const title = 'Lorem ipsum dolor sit amet';
 
 export default function HomePage(props) {
-    // const loadPosts = useCallback(async () => {
-    //     return await getPosts();
-    // });
+    const [isOpen, setIsOpen] = useState(false);
+    // const [posts, setPosts] = useState([]);
 
-    const loadPosts = function() {
-        return [{id: 5, title: '343', content: 'jfdf', containsBadWords: false}];
+    const loadPosts = async function() {
+        return getPosts();
     }
 
     const submitPost = useCallback(async (title, content) => {
-        await createPost(title, content);
-    })
+        // await createPost(title, content);
+    }, []);
 
     return (
-        <div className={"home-page"}>
-            <PostCreation submitPost={submitPost}></PostCreation>
-            <Feed posts={loadPosts()}/>
+        <div className={"home-page"} style={{'padding-top': '50px'}}>
+            {isOpen && <PostCreation submitPost={submitPost}/>}
+            {!isOpen && <Feed posts={[{content: text, title: title, id:5, containsBadWords: true}]}/>}
+            {!isOpen && <Button onClick={() => {setIsOpen(true)}}>צור פוסט חדש</Button>}
         </div>
     )
 }
