40d0bfa5b31520c2a6dc6af4bb3f5a842b94662a
Roy Gordon <royg2000@gmail.com>
2020-10-09 00:40:59 +0300

    Created a basic post component

--- ilistener/src/pages/homePage.js
+++ ilistener/src/pages/homePage.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { mainTheme } from '../styling/themes';
+import Post from '../components/post';
+// import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+// import Button from '@material-ui/core/Button';
+// import { mainTheme } from '../styling/themes';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundColor: theme.palette.primary.main,
-        '&:hover': {
-            backgroundColor: theme.palette.primary.dark
-        },
-        '&:active': {
-            backgroundColor: theme.palette.primary.light
-        }
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         backgroundColor: theme.palette.primary.main,
+//         '&:hover': {
+//             backgroundColor: theme.palette.primary.dark
+//         },
 
-    },
-}))
+//     },
+// }))
+
+//Todo: Remove later
+const tempMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const tempTitle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
 
 export default function HomePage(props) {
-    const classes = useStyles();
+    // const classes = useStyles();
+
+    // <ThemeProvider theme={mainTheme} >
+    //     <Button classes={{...classes}}>
+    //         Hello world...
+    //     </Button>
+    // </ThemeProvider>
 
     return (
-        <ThemeProvider theme={mainTheme} >
-            <Button classes={{
-                root: classes.root,
-                hover: classes.hover,
-                pressed: classes.pressed,
-            }}>
-                Hello world...
-            </Button>
-        </ThemeProvider>
+        <Post 
+            isOffensive={true}
+            content={tempMessage}
+            title={tempTitle}
+            id={1}
+        />
     )
 }
