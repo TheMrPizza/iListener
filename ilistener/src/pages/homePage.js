d0818383dc969eeef2f97f5a6536cf0b63594b99
Roy Gordon <royg2000@gmail.com>
2020-10-08 22:17:17 +0300

    Added the base for custom themes

--- ilistener/src/pages/homePage.js
+++ ilistener/src/pages/homePage.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { mainTheme } from '../styling/themes';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: theme.palette.primary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.primary.dark
+        },
+        '&:active': {
+            backgroundColor: theme.palette.primary.light
+        }
+
+    },
+}))
 
 export default function HomePage(props) {
+    const classes = useStyles();
+
     return (
-        <div>
-            Hello world...
-        </div>
+        <ThemeProvider theme={mainTheme} >
+            <Button classes={{
+                root: classes.root,
+                hover: classes.hover,
+                pressed: classes.pressed,
+            }}>
+                Hello world...
+            </Button>
+        </ThemeProvider>
     )
 }
